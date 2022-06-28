29b0a769fa1c8ef8ac26ae4834aa8403ba944987
Usama Fazal <Usama Fazal@WIN-VOL3D49H6UK>
2022-06-29 00:46:51 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace QuestionNo9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
